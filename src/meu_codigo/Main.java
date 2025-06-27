ad3904c323f1479deacca757d63893ea75a07a6c
SpanishCodes4Life <milanesartsobrabo@gmail.com>
2025-06-26 21:59:43 -0300

    Primeiro commit em Java.

---
+++ src/meu_codigo/Main.java
@@ -0,0 +1,40 @@
+package meu_codigo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Olá,Mundo Cruel!");
+		System.out.println("Vai se foder Elon Musk");
+		System.out.println("Maluco Beleza de cara na Trave");
+		System.out.print("Minha Idade é: ");
+		
+		System.out.println(15+5);
+		
+		String MeuNome = "Joel";
+		String Curso = "Eu Estudo de Sistema de Informação";
+		
+		System.out.print("Meu Nome é ");
+		System.out.println(MeuNome);
+		
+		System.out.println(Curso);
+		
+		int IniciodoCurso = 2025;
+		int TerminodoCurso = 2029;
+		int diadehoje = 26;
+		
+		System.out.print("Termino do Curso em: ");
+		System.out.print(TerminodoCurso - IniciodoCurso);
+		System.out.println(" Anos.");
+		System.out.print("Hoje é dia ");
+		System.out.print(diadehoje);
+		System.out.println(" De Junho de 2025");
+		System.out.print("Amanhã é dia: ");
+		
+		diadehoje = diadehoje+1;
+		
+		System.out.print(diadehoje);
+		System.out.println(" De Junho de 2025");
+		
+	}
+
+}
---
+++ src/meu_codigo/segundocódgio.java
@@ -0,0 +1,24 @@
+package meu_codigo;
+
+public class segundocódgio {
+
+	public static void main(String[] args) {
+		
+		int anodenascimento = 2005;
+		int anoatual = 2025;
+		String mesdenascimento = "Julho";
+		int diadenascimento = 25;
+		
+		System.out.println("Nasci no Ano de "+anodenascimento);
+		System.out.println("Do mês de "+mesdenascimento);
+		System.out.println("No dia "+diadenascimento);
+		
+		System.out.println("Estamos no Ano "+ anoatual);
+		
+		int minhaidade = anoatual - anodenascimento;
+		
+		System.out.println("Minha Idade é "+minhaidade);
+		
+		System.out.println("Qual é Meu Nome?");
+	}
+}
